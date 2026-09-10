954c1e686e1b30a6c63947fb90baab3b5ad64775
kyia-06 <krystalclairefabic@gmail.com>
2026-09-10 11:38:54 +0800

    Add singular user route alias

--- app/config/routes.php
+++ app/config/routes.php
@@ -46,6 +46,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $router->get('/', 'Welcome::index');
 $router->get('/users', 'UsersController::index');
+$router->get('/user', 'UsersController::index');
 
 $router->get('/login', 'AuthController::login');
 $router->post('/login', 'AuthController::authenticate');
